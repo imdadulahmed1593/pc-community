67e9b5f8ad0e7cd39c90ad2b73fb314a1293ff31
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2023-11-01 05:07:17 +0600

    feat: server creation and auth

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,28 @@
+import { redirect } from 'next/navigation';
+
+import { db } from '@/lib/db';
+import { initialProfile } from '@/lib/initial-profile';
+import { InitialModal } from '@/components/modals/initial-modal';
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+  // console.log(profile);
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  console.log(profile.id, server);
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModal />;
+};
+
+export default SetupPage;
