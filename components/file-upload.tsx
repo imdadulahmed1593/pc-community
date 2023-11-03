eb026af86a2121094676629f19dd394a42d65265
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2023-11-03 14:59:40 +0600

    uploadthing bug fix

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,50 +1,41 @@
-"use client";
+'use client';
 
-import { FileIcon, X } from "lucide-react";
-import Image from "next/image";
+import { FileIcon, X } from 'lucide-react';
+import Image from 'next/image';
 
-import { UploadDropzone } from "@/lib/uploadthing";
+import { UploadDropzone } from '@/lib/uploadthings';
 
-import "@uploadthing/react/styles.css";
+import '@uploadthing/react/styles.css';
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
-  endpoint: "messageFile" | "serverImage"
+  endpoint: 'messageFile' | 'serverImage';
 }
 
-export const FileUpload = ({
-  onChange,
-  value,
-  endpoint
-}: FileUploadProps) => {
-  const fileType = value?.split(".").pop();
+export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split('.').pop();
 
-  if (value && fileType !== "pdf") {
+  if (value && fileType !== 'pdf') {
     return (
       <div className="relative h-20 w-20">
-        <Image
-          fill
-          src={value}
-          alt="Upload"
-          className="rounded-full"
-        />
+        <Image fill src={value} alt="Upload" className="rounded-full" />
         <button
-          onClick={() => onChange("")}
+          onClick={() => onChange('')}
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
           type="button"
         >
           <X className="h-4 w-4" />
         </button>
       </div>
-    )
+    );
   }
 
-  if (value && fileType === "pdf") {
+  if (value && fileType === 'pdf') {
     return (
       <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
         <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
-        <a 
+        <a
           href={value}
           target="_blank"
           rel="noopener noreferrer"
@@ -53,14 +44,14 @@ export const FileUpload = ({
           {value}
         </a>
         <button
-          onClick={() => onChange("")}
+          onClick={() => onChange('')}
           className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
           type="button"
         >
           <X className="h-4 w-4" />
         </button>
       </div>
-    )
+    );
   }
 
   return (
@@ -73,5 +64,5 @@ export const FileUpload = ({
         console.log(error);
       }}
     />
-  )
-}
+  );
+};
